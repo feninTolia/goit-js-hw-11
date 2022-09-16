9dfae7045a80159f1dab6db5a581ad05ccefb9db
feninTolia <fenintolja@gmail.com>
2022-09-16 09:34:43 +0300

    added buttons styles and logic

--- src/js/index.js
+++ src/js/index.js
@@ -3,22 +3,6 @@ import { refs } from './modules/refs';
 import createPhotoCardMarkup from '../templates/photo-card-temp.hbs';
 
 const fetchPhoto = new FetchPhoto();
-let photosAreOver = null;
-
-const renderGallery = enteredQuery => {
-  fetchPhoto
-    .byQuery(enteredQuery)
-    .then(response => {
-      const { data } = response;
-
-      refs.gallery.insertAdjacentHTML('beforeend', createPhotoCardMarkup(data));
-
-      if (data.total < 17) {
-        photosAreOver = true;
-      }
-    })
-    .catch(err => console.log(err));
-};
 
 const renderRandomGallery = () => {
   fetchPhoto
@@ -34,19 +18,36 @@ const renderRandomGallery = () => {
     .catch(err => console.log(err));
 };
 
+const renderGallery = enteredQuery => {
+  fetchPhoto
+    .byQuery(enteredQuery)
+    .then(response => {
+      const { data } = response;
+
+      refs.gallery.insertAdjacentHTML('beforeend', createPhotoCardMarkup(data));
+
+      if (data.hits.length < 16) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+      }
+    })
+    .catch(err => console.log(err));
+};
+
 const onFormSubmit = e => {
   e.preventDefault();
   const enteredQuery = e.target.searchQuery.value;
+  e.target.lastElementChild.disabled = true;
+  refs.searchBtn.classList.remove('.search-btn:hover');
 
   fetchPhoto.page = 0;
   refs.gallery.innerHTML = '';
   renderGallery(enteredQuery);
 
   refs.loadMoreBtn.classList.remove('is-hidden');
+};
 
-  if (photosAreOver) {
-    refs.loadMoreBtn.classList.add('is-hidden');
-  }
+const onSearchQueryInput = () => {
+  refs.searchForm.lastElementChild.disabled = false;
 };
 
 const onLoadMoreBtnClick = e => {
@@ -55,6 +56,6 @@ const onLoadMoreBtnClick = e => {
 };
 
 renderRandomGallery();
-
 refs.searchForm.addEventListener('submit', onFormSubmit);
+refs.searchForm.searchQuery.addEventListener('input', onSearchQueryInput);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
